e2e314474c248a74229f4d06c7d357b00bbaf6d2
Alexey Avramov <hakavlad@gmail.com>
2018-06-09 22:13:01 +0900

    исправлен алгоритм, улучшен вывод, добавлены опции командной строки, дополнен конфиг, дополнен ман

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -v
 
-cp nohang /usr/local/bin/
+cp -f nohang /usr/local/bin/
 chmod 755 /usr/local/bin/nohang
 
 mkdir /etc/nohang
 chmod 755 /etc/nohang
-cp nohang.conf /etc/nohang/
+cp -f nohang.conf /etc/nohang/
 chmod 644 /etc/nohang/nohang.conf
 
 gzip -k nohang.1
 mkdir /usr/local/share/man/man1
 chmod 755 /usr/local/share/man/man1
-cp nohang.1.gz /usr/local/share/man/man1/
+cp -f nohang.1.gz /usr/local/share/man/man1/
 chmod 644 /usr/local/share/man/man1/nohang.1.gz
 
-cp nohang.service /etc/systemd/system/
+cp -f nohang.service /etc/systemd/system/
 chmod 644 /etc/systemd/system/nohang.service
 systemctl daemon-reload
 systemctl enable nohang
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -v
 systemctl stop nohang
 systemctl disable nohang
-rm /usr/local/share/man/man1/nohang.1.gz
-rm /etc/systemd/system/nohang.service
-rm /usr/local/bin/nohang
+rm -f /usr/local/share/man/man1/nohang.1.gz
+rm -f /etc/systemd/system/nohang.service
+rm -f /usr/local/bin/nohang
