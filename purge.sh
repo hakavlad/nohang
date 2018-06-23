8825021f6b867d5c01015b40eaf5f466c0c2ee49
Alexey Avramov <hakavlad@gmail.com>
2018-06-23 15:39:17 +0900

    add support white, black, avoid, prefer lists

--- purge.sh
+++ purge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -v
 systemctl stop nohang
 systemctl disable nohang
-rm -f /usr/local/share/man/man1/nohang.1.gz
-rm -f /etc/systemd/system/nohang.service
-rm -rf /etc/nohang
-rm -f /usr/local/bin/nohang
+rm /usr/local/share/man/man1/nohang.1.gz
+rm /etc/systemd/system/nohang.service
+rm -r /etc/nohang
+rm /usr/local/bin/nohang
